d2e93f30ed628e37333ced7fbade0217f6bfee04
HelenBrush <hln.brush@gmail.com>
2017-07-20 16:23:00 -0400

    Update final.js

--- final.js
+++ final.js
@@ -6,11 +6,23 @@ $("#search").click(function(){
         }
     if (input==="indian grass"){
         $("#indianimg").css("border","solid yellow 20px");
-    }
+        }
         
+    if (input==="milkweed"){
+        $("#milkweedimg").css("border","solid yellow 20px");
+    }
         });
         
         
 $("#beebalmimg").click(function(){
     $("#beebalmimg").css("border","solid black 10px");
 });
+
+$("#indianimg").click(function(){
+    $("#indianimg").css("border","solid black 10px");
+});
+
+$("#milkweedimg").click(function(){
+    $("#milkweedimg").css("border","solid black 10px");
+});
+
