b3144efe1e5110e96229a21c7ab4db9bdaecd37b
HelenBrush <hln.brush@gmail.com>
2017-07-21 14:05:55 -0400

    Update final.js

--- final.js
+++ final.js
@@ -27,6 +27,26 @@ $("#search").click(function(){
      if (input==="thimbleweed"){
         $("#thimbleweedimg").css("border","solid yellow 20px");
     }
+    
+     if (input==="nodding wild onion"){
+        $("#noddingimg").css("border","solid yellow 20px");
+    }
+    
+     if (input==="coreopsis"){
+        $("#coreopsisimg").css("border","solid yellow 20px");
+    }
+    
+     if (input==="lupine"){
+        $("#lupineimg").css("border","solid yellow 20px");
+    }
+    
+    if (input==="helenium"){
+        $("#heleniumimg").css("border","solid yellow 20px");
+    }
+    
+    if (input==="boneset"){
+        $("#bonesetimg").css("border","solid yellow 20px");
+    }
         });
         
         
@@ -58,4 +78,23 @@ $("#thimbleweedimg").click(function(){
     $("#thimbleweedimg").css("border","solid black 10px");
 });
 
+$("#noddingimg").click(function(){
+    $("#noddingimg").css("border","solid black 10px");
+});
 
+$("#coreopsisimg").click(function(){
+    $("#coreopsisimg").css("border","solid black 10px");
+});
+
+$("#lupineimg").click(function(){
+    $("#lupineimg").css("border","solid black 10px");
+});
+
+$("#heleniumimg").click(function(){
+    $("#heleniumimg").css("border","solid black 10px");
+});
+    
+$("#bonesetimg").click(function(){
+    $("#bonesetimg").css("border","solid black 10px");
+});
+    
