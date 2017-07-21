f313020a5d019243885b01db017e52ca57bdb352
HelenBrush <hln.brush@gmail.com>
2017-07-21 11:47:15 -0400

    Update final.js

--- final.js
+++ final.js
@@ -11,6 +11,22 @@ $("#search").click(function(){
     if (input==="milkweed"){
         $("#milkweedimg").css("border","solid yellow 20px");
     }
+    
+      if (input==="black eyed susan"){
+        $("#blackeyedimg").css("border","solid yellow 20px");
+    }
+    
+     if (input==="coneflower"){
+        $("#coneflowerimg").css("border","solid yellow 20px");
+    }
+    
+     if (input==="columbine"){
+        $("#columbineimg").css("border","solid yellow 20px");
+    }
+    
+     if (input==="thimbleweed"){
+        $("#thimbleweedimg").css("border","solid yellow 20px");
+    }
         });
         
         
@@ -25,4 +41,21 @@ $("#indianimg").click(function(){
 $("#milkweedimg").click(function(){
     $("#milkweedimg").css("border","solid black 10px");
 });
+
+$("#blackeyedimg").click(function(){
+    $("#blackeyedimg").css("border","solid black 10px");
+});
+
+$("#coneflowerimg").click(function(){
+    $("#coneflowerimg").css("border","solid black 10px");
+});
+
+$("#columbineimg").click(function(){
+    $("#columbineimg").css("border","solid black 10px");
+});
+
+$("#thimbleweedimg").click(function(){
+    $("#thimbleweedimg").css("border","solid black 10px");
+});
+
 
