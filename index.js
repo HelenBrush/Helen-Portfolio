52256937aac74f2d8d37884126774d6274d05211
HelenBrush <hln.brush@gmail.com>
2017-07-21 13:07:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-  var change=false;
+//  var change=false;
 
-$("body").click(function(){
-    if(change===false){
-        document.body.style.backgroundImage = "url('https://mail.google.com/mail/u/0/?ui=2&ik=21b75b9a14&view=att&th=15d5d797e8daa16f&attid=0.4&disp=safe&realattid=f_j5bpxs393&zw')";
-        change=true;
-    }
-    if(change===true){
-           document.body.style.backgroundImage = "url('https://mail.google.com/mail/u/0/?ui=2&ik=21b75b9a14&view=att&th=15d5d797e8daa16f&attid=0.1&disp=safe&realattid=f_j5bpvwov0&zw')";
-            change=false;
-    }
+//$("body").click(function(){
+    //if(change===false){
+       // document.body.style.backgroundImage = "url('https://mail.google.com/mail/u/0/?ui=2&ik=21b75b9a14&view=att&th=15d5d797e8daa16f&attid=0.4&disp=safe&realattid=f_j5bpxs393&zw')";
+       // change=true;
+  //  }
+   // if(change===true){
+         //  document.body.style.backgroundImage = "url('https://mail.google.com/mail/u/0/?ui=2&ik=21b75b9a14&view=att&th=15d5d797e8daa16f&attid=0.1&disp=safe&realattid=f_j5bpvwov0&zw')";
+          //  change=false;
+  //  }
     
+//});
+$("#pic1").click(function(){
+    document.body.style.backgroundImage = "url('https://mail.google.com/mail/u/0/?ui=2&ik=21b75b9a14&view=att&th=15d5d797e8daa16f&attid=0.1&disp=safe&realattid=f_j5bpvwov0&zw')";
 });
- 
+
+
+$("#pic2").click(function(){
+    document.body.style.backgroundImage = "url('https://mail.google.com/mail/u/0/?ui=2&ik=21b75b9a14&view=att&th=15d5d797e8daa16f&attid=0.6&disp=safe&realattid=f_j5bpzp7u5&zw')";
+});
+
+$("#pic3").click(function(){
+    document.body.style.backgroundImage = "url('https://mail.google.com/mail/u/0/?ui=2&ik=21b75b9a14&view=att&th=15d5d797e8daa16f&attid=0.3&disp=safe&realattid=f_j5bpxd1j2&zw')";
+});
+
+
+$("#pic4").click(function(){
+    document.body.style.backgroundImage = "url('https://mail.google.com/mail/u/0/?ui=2&ik=21b75b9a14&view=att&th=15d5d797e8daa16f&attid=0.5&disp=safe&realattid=f_j5bpyi4f4&zw')";
+});
